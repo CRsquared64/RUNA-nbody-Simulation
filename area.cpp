b21b3b79b16699f7c6c28b89311ee7f0ad1d7790
callum <Callumr714@icloud.com>
2024-04-27 14:07:19 +0100

    Created Area, Node, Quadtree files

---
+++ area.cpp
@@ -0,0 +1,9 @@
+struct area
+{
+    int min_x, min_y, max_x, max_y;
+    area(int Min_x, int Min_y, int Max_x, int Max_y){
+        min_x = Min_x; min_y = Min_y; max_x = Max_x; max_y = Max_y;
+
+    }
+
+};
---
+++ quadTree.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+#include "area.cpp"
+
+
+class quadTree
+{
+private:
+    
+public:
+    int max_depth = 128;
+
+  
+};
+
