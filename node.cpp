8b4356e7122bdd9cfcf7628f60d515164789dceb
callum <Callumr714@icloud.com>
2024-09-08 15:28:02 +0100

    NODE NODE NODE NODE AHHH

---
+++ node.cpp
@@ -0,0 +1,66 @@
+#pragma once
+#include <iostream>
+#include <variant>
+#include <array>
+#include "nbody.cpp"
+#include "area.cpp"
+
+class Node
+{
+public:
+    Area area;
+    int depth;
+    using ChildrenVariant = std::variant<Nbody, Node>; // holds objects
+    std::array<std::optional<ChildrenVariant>, 4> childrenArray;  // Use std::optional to delay initialization
+
+    double x = 0;
+    double y = 0;
+    double mass = 0;
+
+
+    Node(double min_x, double min_y, double max_x, double max_y, int depth)
+        : area(min_x, min_y, max_x, max_y), depth(depth)
+    {
+        childrenArray.fill(std::nullopt); // fill it with NOTHING
+    }
+    
+
+    Node() : area(0, 0, 0, 0), depth(0)
+    {
+        childrenArray.fill(std::nullopt);
+    }
+
+    void update_mass()
+    {
+        int count = 0;
+        double massTemp = 0.0;
+        double xTemp = 0.0;
+        double yTemp = 0.0;
+
+        for (auto &child : childrenArray)
+        {
+            if (child)
+            {
+                std::visit([&](auto &childObj)
+                {
+                    xTemp+= childObj.x;
+                    yTemp += childObj.y;
+
+                    massTemp += childObj.mass;
+                    count++;
+                }, *child);
+
+            }
+        }
+        if (count != 0)
+        {
+            x = xTemp / count;
+            y = yTemp / count;
+
+            mass = massTemp;
+
+
+         }
+
+    }   
+};
